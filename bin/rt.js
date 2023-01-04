29c90bea19302f1165b599fd907b20a3654fcee2
william <chai@strikingly.com>
2023-01-04 15:48:56 +0800

    feat: update main js path

--- bin/rt.js
+++ bin/rt.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict'
-const cli = require('../dist/cli') //src
+const cli = require('../src/cli') //src
 // console.log(process.argv);
 const exitCode = cli.execute(process.argv)
 /*eslint no-process-exit:0*/
--- playground/rt-main.js
+++ playground/rt-main.js
@@ -2,7 +2,7 @@
 'use strict'
 /*eslint-env browser*/
 /*var _ = */require('lodash')
-var reactTemplates = require('../dist/reactTemplates')
+var reactTemplates = require('../src/reactTemplates')
 window.reactTemplates = reactTemplates
 
 
